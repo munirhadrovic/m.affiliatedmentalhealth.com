b6f2cc2e7082062211853737ff8af2233fd2e600
neswill1 <nenadkonatar@live.ac.me>
2015-02-04 16:41:45 +0100

    Nenad - Second Version

--- settings.php
+++ settings.php
@@ -3,6 +3,6 @@
 	$grad="Seattle";//jako je vazno ako adresa ima dva imena povezati ih sa plus
 	$skracenica="WA"; // AKo ne znate skracenicu ostavite praznu
 	$telephone=2063227621;// Broj koji se poziva na dugme CALL US
-	$theme="c";//Thema koja se aplicira na sve
+	$theme="a";//Thema koja se aplicira na sve
 	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
 ?>
